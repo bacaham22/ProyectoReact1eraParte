98628455d785e6d6954c68130337b570ae96de73
bacaham22 <carlosdanielak@outlook.com>
2021-07-16 20:31:18 -0500

    version 3.0-React con Bootstrap

--- src/components/C02contador.js
+++ src/components/C02contador.js
@@ -1,4 +1,4 @@
-import React from 'react'   //rafce
+/*import React from 'react'   //rafce
 
 const c02contador = () => {
     return (
@@ -10,3 +10,4 @@ const c02contador = () => {
 }
 
 export default c02contador
+*/
