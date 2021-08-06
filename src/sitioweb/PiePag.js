a710f8be19a49d02eaadabbbdf84e3a1d9f4dc40
bacaham22 <carlosdanielak@outlook.com>
2021-08-06 09:16:50 -0500

    Version 6-Ultimate

--- src/sitioweb/PiePag.js
+++ src/sitioweb/PiePag.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PiePag = () => {
     return (
         <div className="d-flex align-items-center ps-5" style={{height:120 }}>
-            <h3>Información del sitio web
+            <h3>ESTUDIANTE: Alvarez Kana, Carlos Daniel
             </h3>
         </div>
     )
